e68a39adc7ca555b1371e13a5989b9bd0cb60ffa
Benedictus Mwanthi <benedictusyohana5@gmail.com>
2022-03-16 14:49:16 +0300

    user input validation

--- src/main/java/ceasercipher.java
+++ src/main/java/ceasercipher.java
@@ -9,6 +9,7 @@ public class ceasercipher {
         getInputAndRunCaesar();
     }
     //accept input from the user, encrypt and decrypt if acceptable, terminate if unacceptable
+
     public static void getInputAndRunCaesar() {
         int shift = 0;
         String message;
@@ -16,9 +17,19 @@ public class ceasercipher {
         String origMessage;
         Scanner strReader = new Scanner(System.in);
         Scanner intReader = new Scanner(System.in);
+
         //get message to encode from user
         System.out.println("Please enter the text you wish to encrypt. No numeric values are permitted!");
         System.out.println("You may use upper or lower case, and acceptable punctuation marks are '.' ',' '!' and '?'.");
         System.out.print("Your message: ");
         message = strReader.nextLine();
+        //ensure that input message contains no numeric values and is not empty
+        if(message.matches(".*\\d.*")){
+            System.out.println("Your input does not meet the criteria stated above.");
+            System.exit(0);
+        }else if(message.equals("")){
+            System.out.println("You didn't enter anything.");
+            System.exit(0);
+        }
+    }
 }
