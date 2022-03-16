2edc703887db3c0c19aded5e5a83345dd4a16dc0
Benedictus Mwanthi <benedictusyohana5@gmail.com>
2022-03-16 14:46:48 +0300

    Accept User Input

--- src/main/java/ceasercipher.java
+++ src/main/java/ceasercipher.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class ceasercipher {
+    private static String message;
+    private static int shift;
+
+    //main driver of the program
+    public static void main(String[] args) {
+        getInputAndRunCaesar();
+    }
+    //accept input from the user, encrypt and decrypt if acceptable, terminate if unacceptable
+    public static void getInputAndRunCaesar(){
+        int shift = 0;
+        String message;
+        String encMessage;
+        String origMessage;
+        Scanner strReader = new Scanner(System.in);
+        Scanner intReader = new Scanner(System.in);
 }
