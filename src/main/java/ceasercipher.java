4ee7fb1349de713688b32d2446049c02ffefd820
Benedictus Mwanthi <benedictusyohana5@gmail.com>
2022-03-16 14:48:07 +0300

    Get encoding message

--- src/main/java/ceasercipher.java
+++ src/main/java/ceasercipher.java
@@ -9,11 +9,16 @@ public class ceasercipher {
         getInputAndRunCaesar();
     }
     //accept input from the user, encrypt and decrypt if acceptable, terminate if unacceptable
-    public static void getInputAndRunCaesar(){
+    public static void getInputAndRunCaesar() {
         int shift = 0;
         String message;
         String encMessage;
         String origMessage;
         Scanner strReader = new Scanner(System.in);
         Scanner intReader = new Scanner(System.in);
+        //get message to encode from user
+        System.out.println("Please enter the text you wish to encrypt. No numeric values are permitted!");
+        System.out.println("You may use upper or lower case, and acceptable punctuation marks are '.' ',' '!' and '?'.");
+        System.out.print("Your message: ");
+        message = strReader.nextLine();
 }
